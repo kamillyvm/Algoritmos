52db2becd2d0592d4a714b4cb18dcb55b3007012
Kamilly Medino <148277123+kamillyvm@users.noreply.github.com>
2025-06-11 10:38:47 -0300

    update

---
+++ Lista 4/L4Q7.cpp
@@ -0,0 +1,33 @@
+/*
+
+Elabore um algoritmo que leia dois vetores V1 e V2 de 10 posições
+cada e faça a multiplicação dos elementos de mesmo índice, colocando
+o resultado em um terceiro vetor V3. Mostre o vetor resultante.
+
+*/
+
+#include <iostream>
+using namespace std;
+
+int main(){
+    int v1[10], v2[10], v3[10];
+    int i;
+    
+    cout << "10 elementos cada: " << endl;
+    for(i=0; i<10; i++){
+    	cout << "digite o primeiro elemento para multiplicar: ";
+        cin >> v1[i];
+        cout << "digite o segundo elemento para multiplicar: ";
+		cin >> v2[i];
+        
+        v3[i] = v1[i]*v2[i];
+        
+    }
+    
+    cout << "\nreultado: ";
+    for(i=0; i<10; i++){
+        cout << v3[i] << " ";
+    }
+
+    return 0;
+}
